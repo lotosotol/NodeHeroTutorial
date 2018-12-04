76e62fea8c944e05c8c028b759862016965976c2
valeriu.jecov <valeriu.jecov@ucalgary.ca>
2018-12-04 14:32:33 -0700

    [VJ] first commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+// app/index.js
+const calc = require('./calc');
+
+const numbersToAdd = [3, 4, 10, 2];
+
+const result = calc.sum(numbersToAdd);
+
+console.log(`The result is: ${result}`);
---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+require('./app/index');
+
+// console.log('hello from Node.js');
+
+//proj on github:
+// https://github.com/lotosotol/NodeHeroTutorial.git
+// git@github.com:lotosotol/NodeHeroTutorial.git
